5a09e533915a143b8831e105deb050f0f859acc1
nilaio <344298947@qq.com>
2020-10-25 14:49:06 +0800

    preivous version

--- visual_test/test_only.py
+++ visual_test/test_only.py
@@ -7,6 +7,9 @@ import json
 val_data_root = "../data/official_data/valdata/"
 val_json = "../data/official_data/val.json"
 
+test_data_root = "../data/official_data/testdata/"
+test_json = "../data/official_data/test.json"
+
 def widder(im):
 
     row, col = im.shape[:2]
@@ -26,11 +29,11 @@ def widder(im):
 
     return border
 
-def trans1():
-    for item in os.listdir(val_data_root):
+def trans1(data_root, json_file):
+    for item in os.listdir(data_root):
         val_dict = {}
-        img_open = cv2.imread(val_data_root + item, 0)
-        print(item + "- size: " + str(img_open.shape))
+        img_open = cv2.imread(data_root + item, 0)
+        print(item + " - size -  " + str(img_open.shape))
         if img_open.shape[0] < 18:
             img_open = widder(img_open)
         #img_open = cv2.imread("./train_4.jpg", 0)
@@ -47,11 +50,12 @@ def trans1():
                 continue
             else:
                 prediction_string = prediction_string + prediction[i] + " "
+                # prediction_string = prediction_string + prediction[i]
         print(prediction_string)
         val_dict['filename'] = str(item)
         val_dict['result'] = str(prediction_string)
-        with open(val_json, 'a+') as f_val:
+        with open(json_file, 'a+') as f_val:
             json.dump(val_dict, f_val)
 
 if __name__ == "__main__":
-    trans1()
+    trans1(test_data_root, test_json)
